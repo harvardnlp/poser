eb782a714331d1ca0533ca5dfd26628f072b60da
Sasha Rush <srush.research@gmail.com>
2021-07-09 16:16:29 -0400

    run

--- start.py
+++ start.py
@@ -9,7 +9,7 @@ import numpy as np
 import torch
 __st.set_option('deprecation.showPyplotGlobalUse', False)
 
-poses = json.load(open("p1.json"))
+poses = json.load(open("p3.json"))
 
 epsilon = poses["epsilon"]
 figure = poses["figure"]
@@ -34,6 +34,26 @@ npts = original.shape[0]
 graph = torch.tensor(graph)
 parameters = original.clone()
 parameters.requires_grad_(True)
+
+
+
+def find_intersections(p):
+    bad_edges = []
+    vertices = [sg.Point2(a[0], a[1]) for a in p.detach().numpy()]
+    edge_segments = [sg.Segment2(vertices[fro], vertices[to])
+                     for (fro, to) in graph
+                     if poly.oriented_side(vertices[fro]) == sg.Sign.POSITIVE and poly.oriented_side(vertices[to]) == sg.Sign.POSITIVE]
+    
+    for i, edge_segment in enumerate(edge_segments):
+        for edge in poly.edges:
+            if sg.intersection(edge, edge_segment):
+                bad_edges.append(i)
+                break
+    return bad_edges
+
+x = find_intersections(parameters)
+x
+
 def seg_dist(p):
     constraint = p[graph[:, 0]] - p[graph[:, 1]]
     return (constraint * constraint).sum(-1)
@@ -85,6 +105,29 @@ def outside_constraint(points):
     w = end_hole = holepts[list(range(1, s)) + [0]]
     return euclidean_projection(p, v, w).min(-1)[0], p, outer
 
+def random_constraint(points):
+    intersections = find_intersections(points)
+    new_points = []
+    r = torch.rand(100, 1, 1)
+    points = r * points[graph[intersections, 0]]  + (1-r) * points[graph[intersections,1]]
+
+    outer = []
+    for i in range(points.shape[1]):
+        outer_for_i = []
+        for r in range(points.shape[0]):
+            if poly.oriented_side(sg.Point2(points[r, i, 0], points[r, i, 1])) == sg.Sign.NEGATIVE:
+                outer_for_i.append(i)
+                if len(outer_for_i) > 10:
+                    break
+        outer += outer_for_i
+    p = outer_points = points[outer]
+    
+    s = holepts.shape[0]
+    v = start_hole = holepts[:]
+    w = end_hole = holepts[list(range(1, s)) + [0]]
+    return euclidean_projection(p, v, w).min(-1)[0], intersections, outer
+
+
 def inside_constraint(points):
     inside = [i for i, p in enumerate(points)
              if poly.oriented_side(sg.Point2(p[0], p[1])) == sg.Sign.POSITIVE]
@@ -112,11 +155,12 @@ def objective(p):
     return constraint.min(-1)[0].sum()
 
 lambd = torch.ones([1], requires_grad=True)
-rate = 0.2
+rate = 0.5
 opt = torch.optim.SGD([parameters], lr=rate)
 if True:
+    success = False
     for epochs in range(10000):
-        if (epochs % 1000) == 0 or epochs == 999:
+        if (epochs % 1000) == 0 or epochs == 999 or success:
             plt.clf()
             draw(poly)
             vertices = [sg.Point2(a[0], a[1]) for a in parameters.detach().numpy()]
@@ -125,24 +169,29 @@ if True:
             for segment in edge_segments:
                 draw(segment)
             __st.pyplot()
-
+            # if success == True:
+            #     break
+        success = False
         opt.zero_grad()
         spring_cons = spring_constraint(parameters).mean()
         st_cons = stretch_constraint(parameters).sum()
         outside_cons, violators, v_index = outside_constraint(parameters)
+        random_cons, intersections, outer = random_constraint(parameters)
         inside_cons, _, _ = inside_constraint(parameters)
+        inside_cons = inside_cons.clamp(max=12)
         int_cons = (parameters - parameters.round()).abs().mean()
         # loss = objective(parameters) + st_cons + outside_cons
 
-        if epochs < 8000:
-            loss = -0.02 * inside_cons.sum() + 0.1 * outside_cons.sum() +  spring_cons  + st_cons 
+        if epochs < 5000:
+            loss = -0.05 * inside_cons.sum() + random_cons.sum() + 0.1 * outside_cons.sum() +  spring_cons  + st_cons 
         else:
-            loss =  0.1 * outside_cons.sum() + st_cons + 0.01 * spring_cons
+            loss =   0.1 * outside_cons.sum() + spring_cons
         loss.backward()
         delta = (rate * parameters.grad)
         parameters.data -= delta
-        if epochs > 8000:
-            decay = 0.01
+        if True:
+            # opt.lr = rate * 0.5
+            decay = 0.02
             roundies = (parameters.data - parameters.data.round()).abs() <= decay
             parameters.data[roundies] =  parameters.data[roundies].round()
             noroundies = (parameters.data - parameters.data.round()).abs() > decay
@@ -155,12 +204,18 @@ if True:
         #         if stretch_constraint(parameters).sum() == 0.0:
         #             break
         #         parameters.data[ptindex] += delta
-            
+        p = parameters.detach().round()
+        if stretch_constraint(p).sum().item() == 0.0 and outside_constraint(p)[0].sum().item() == 0.0:
+            __st.write("success!")
+            __st.write(p)
+            print({"vertices" : [[int(t[0].item()), int(t[1].item())] for t in p]})
+            success = True
         if epochs % 100 == 0.0:
             __st.write("Epoch", epochs)
             p = parameters.round()        
             d = {"loss": loss.detach().item(),
                  "spring" : spring_cons.detach().item(),
+                 "intersections" : intersections,
                  "stretch" : st_cons.detach().item(),
                  "outside" : outside_cons.sum().detach().item(),
                  "int cons" : int_cons.item(),
